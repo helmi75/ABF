e12555913ba7f721efb6f5d29b599561f77e6066
helmi <helmichiha@gmail.com>
2021-12-08 12:31:22 +0100

    " premier commit"

---
+++ app.py
@@ -0,0 +1,422 @@
+import requests
+from  features import *
+import streamlit as st
+import json
+import pandas as pd 
+import seaborn as sns
+import io
+from millify import millify 
+from millify import millify
+sns.set()
+
+# configuration 
+url = "https://parazun-amazon-data.p.rapidapi.com/search/"
+headers = {
+    'x-rapidapi-host': "parazun-amazon-data.p.rapidapi.com",
+    'x-rapidapi-key': "f2b69e8ab2mshc6b37f3259eace5p104495jsnc2a8ef50efc6"
+    }
+list_pays = ['FR']
+nbr_pages = 2
+
+
+
+add_selectbox = st.sidebar.selectbox('Faire votre choix ',
+                                      ('Chrome jungle Zboub',
+                                      'Api Jungule Zboub',
+                                      'Comparer plusiseurs produits',
+                                      'Analyse Excel',
+                                      'Analyse de groupe'
+                                      ))
+
+st.title('Jungle Zboub')
+couleurs = ['#D56149','#49B5D5','yellow','green']#,'gray','pink','orange']  
+# Premère page 'Api Jungule Zboub'
+if  (add_selectbox == 'Api Jungule Zboub'):
+  search  = st.text_input('entrer un nom de produit')
+  if search :
+    dict_reponse = scap_data(search, list_pays ,nbr_pages ,url ,headers)
+    list_concat =[]
+    for elm in dict_reponse:
+      #cleanning dataset drop nan 
+      globals() ['df_cleaned_'+ elm ]= cleaning(dict_reponse[elm])
+      list_concat.append(globals() ['df_cleaned_'+ elm ])
+
+    #plot data on scatter and boplot 
+    df_util= pd.concat(list_concat,axis=0)
+
+    # affichage de dataframe
+    st.dataframe(df_util)
+    st.subheader(f'Nbr de concurents :  {df_util.shape[0]}')
+
+    #affichage de scatter
+    st.markdown('**Moyenne etoiles ** en fonction du **Prix**.')
+
+    fig_plotly = affichage(df_util ,'FR','amount','avg_rating')
+    st.plotly_chart(fig_plotly)
+
+    # affiche  boxplot Prix 
+    st.markdown('**Distribution** du **prix mediant**')
+    fig, ax = plt.subplots(figsize=(15,7))
+    ax.boxplot(df_util['amount'],vert=False,patch_artist =True,showmeans =True )
+    st.pyplot(fig)
+
+    # affiche  histograme prix 
+    st.markdown('**Histograme** des **prix **')
+    fig1, ax1 = plt.subplots(figsize=(15,7))
+    ax1.hist(df_util['amount'], bins=40 )
+    st.pyplot(fig1)
+
+    # affiche Histo des  moyenne des etoiles 
+    st.markdown('**Histograme** des **moyenne des etoiles **')
+    fig2, ax2 = plt.subplots(figsize=(15,7))
+    ax2.hist(df_util['avg_rating'], bins=40 )
+    st.pyplot(fig2)
+# Deuxiemme page "Chrome jungle Zboub"
+elif add_selectbox == 'Chrome jungle Zboub':
+  uploaded_file = st.file_uploader("Choose a file")
+
+  if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+    st.header(uploaded_file.name[14:-4]) 
+    df = df.iloc[:,3:]
+
+    # detect data language 
+    langue = dectection_langue(df.columns)  
+    st.write(f'App {langue}')
+
+    # data cleaning 
+    df = preprocessing(df, langue) 
+    st.subheader( f'\nNombre de produit : {df.shape[0]}\n')   
+    
+    
+    
+    
+    # Plot data prosses
+    labels = df['LQS'].value_counts().index
+    sizes =df['LQS'].value_counts().values  
+    st.subheader( '\n\n\n\nAnalyse Univarié')       
+    if langue == 'fr':
+      
+      fig1, (ax1, ax2)  = plt.subplots(1,2,figsize=(12,10))
+      ax1.pie(sizes,  labels=labels, autopct='%1.1f%%', shadow=False, startangle=50)    
+      ax1.set_title( 'Répartition des notes de lising' ,size =20)    
+
+
+      sns.distplot(df['Évaluation'], bins=10)
+      ax2.set_title( 'Notes', size=20) 
+      #st.pyplot(fig1)  
+
+      #ax2.hist(df['Évaluation'],bins=10)
+      #ax2.set_title( 'Histogramme des notes ' ,size =20)
+
+
+      fig2, (ax3, ax4)  = plt.subplots(1,2,figsize=(12,10))
+      df_prix_q075 =  df['Prix'][df['Prix']< df ['Prix'].quantile(0.75)]
+      ax3.boxplot(df_prix_q075 )
+      ax3.set_title(f'\n Prix median {df_prix_q075.median()}€' ,size =20)
+
+      df_Revenus_mensuels_q075 =  df['Revenus mensuels'][df['Revenus mensuels']< df ['Revenus mensuels'].quantile(0.75)]    
+      ax4.boxplot(df_Revenus_mensuels_q075)
+      ax4.set_title(f'Revenue median {df_Revenus_mensuels_q075.median()}€',size =20)
+
+
+      
+      labels_revenu = df['Revenus segementé'].value_counts().sort_index().index
+      values_revenu  = df['Revenus segementé'].value_counts().values   
+
+      fig3, ax5  = plt.subplots(1,1,figsize=(12,5))  
+             
+      ax5.pie(values_revenu ,  labels=labels_revenu, autopct='%1.1f%%', colors=couleurs, startangle=50)
+      ax5.set_title('',size =20)
+
+      fig4, ax6  = plt.subplots(1,1,figsize=(12,5))      
+      sns.scatterplot( df['Classement'],df['Ventes mensuelles'], hue=df['Revenus segementé'])
+      ax6.set_xlim(0,20000)
+      ax6.set_title('Nbr de vente en fonction du classement ',size =20)
+
+      
+      fig5, ax7  = plt.subplots(1,1,figsize=(12,5))      
+      sns.scatterplot(df['Ventes mensuelles'], df['Évaluation'], hue=df['Revenus segementé'])
+      ax7.set_title('influence du chiffre d affaire   M.vente(evalutation)',size =20)
+
+      fig8, ax9  = plt.subplots(1,1,figsize=(12,5))      
+      sns.scatterplot( df['Avis'],df['Classement'], hue=df['Revenus segementé'])
+      ax9.set_xlim(-2,6000)
+      #ax9.set_ylim(-10,5000)
+      ax9.set_title('influence du chiffre d affaire avis(classement)',size =20)
+
+      
+
+    else :
+      fig1, (ax1, ax2)  = plt.subplots(1,2,figsize=(12,10))
+      ax1.pie(sizes,  labels=labels, autopct='%1.1f%%', shadow=False, startangle=50)    
+      ax1.set_title( 'Répartition des notes de lising' ,size =20)
+
+      ax2.hist(df['Rating'],bins=10)
+      ax2.set_title( 'Histogramme des notes ' ,size =20)
+
+      fig2, (ax3, ax4)  = plt.subplots(1,2,figsize=(12,10))
+      df_prix_q075 =  df['Price'][df['Price']< df ['Price'].quantile(0.75)]
+      ax3.boxplot(df_prix_q075 )
+      ax3.set_title( f'\nBoxplot prix :  ' ,size =20)
+
+      df_Revenus_mensuels_q075 =  df['Mo. Sales'][df['Mo. Sales']< df ['Mo. Sales'].quantile(0.75)]    
+      ax4.boxplot(df_Revenus_mensuels_q075)
+      ax4.set_title('Boxplot revenue',size =20)
+
+      
+    st.subheader( '\n\n\n\nAnalyse ')
+    
+
+    st.pyplot(fig1)
+    st.pyplot(fig2)
+    st.pyplot(fig3)
+    st.pyplot(fig4)
+    st.pyplot(fig5)
+    st.pyplot(fig8)
+# 3e page 'Comparer plusiseurs produits'
+elif add_selectbox == 'Comparer plusiseurs produits':
+ 
+  files =st.file_uploader('Choose files ', accept_multiple_files=True)
+  dict_niche={}
+  if files :
+    st.header('Niche à comparer\n\n!\n')
+
+    list_nom = []
+    moyenne_note = [] 
+    median_revenu_mensuel = []
+    median_avis = []
+    median_classement = []
+    median_listing = []
+
+    for elm in files:      
+      nom_file = elm.name[14:-4]
+      dict_niche[nom_file] = pd.read_csv(elm)
+      langue = dectection_langue(dict_niche[nom_file].iloc[:,3:]) 
+      dict_niche[nom_file] = preprocessing(dict_niche[nom_file].iloc[:,3:], langue) 
+
+      dict_niche[nom_file]['LQS']= dict_niche[nom_file]['LQS'].apply(lambda x : str(x))
+      labels_LQS = dict_niche[nom_file]['LQS'].apply(lambda x : str(x)).value_counts().sort_index().index
+      values_LQS  = dict_niche[nom_file]['LQS'].value_counts().sort_index().values      
+
+        
+
+      
+      
+
+      # from french datas 
+      if langue == 'fr':
+
+        # plot configuration 
+        fig1,(ax1, ax2, ax3) = plt.subplots(1,3,figsize=(20, 7))
+        fig1.suptitle(nom_file, size=40)
+        plt.xticks(rotation = 90)
+        
+        # Plot Pie of listing score 
+        ax1.pie(values_LQS ,  labels=labels_LQS, autopct='%1.1f%%', colors=couleurs, startangle=50)    
+        ax1.set_title( 'Lising' ,size=20)   
+        
+        labels_revenu = dict_niche[nom_file]['Revenus segementé'].value_counts().sort_index().index
+        values_revenu  = dict_niche[nom_file]['Revenus segementé'].value_counts().values         
+
+        #plot pie of revenues 
+        ax2.pie(values_revenu, labels = labels_revenu ,autopct='%1.1f%%', colors=couleurs, startangle=50)
+        #sns.distplot(dict_niche[nom_file]['Revenus segementé'],bins=10)           
+        ax2.set_title( 'Revenus ' ,size =20)
+        
+        # Histograme of evaluation 
+        sns.distplot(dict_niche[nom_file]['Évaluation'], bins=10)
+        ax3.set_title( 'Notes', size=20) 
+        st.pyplot(fig1)
+
+        #french
+        list_nom.append(nom_file)
+        moyenne_note.append(dict_niche[nom_file]['Évaluation'].median())
+        median_revenu_mensuel.append(dict_niche[nom_file]['Revenus mensuels'].median())
+        median_classement.append(dict_niche[nom_file]['Classement'].median())
+        median_avis.append(dict_niche[nom_file]['Avis'].median())
+        median_listing.append(dict_niche[nom_file]['LQS'].median())
+
+
+      # from english datas  
+      else:   
+
+        # plot configuration 
+        fig1, (ax1 ,ax2, ax3) = plt.subplots(1, 3, figsize=(20,7))
+        fig1.suptitle(nom_file, size=40)
+        plt.xticks(rotation = 90)    
+
+        # Plot Pie of listing score     
+        ax1.pie(values_LQS ,  labels=labels_LQS, autopct='%1.1f%%', colors=couleurs, startangle=50)    
+        ax1.set_title( 'Listing' ,size =20) 
+
+        labels_revenu = dict_niche[nom_file]['Revenus segemented'].value_counts().sort_index().index
+        values_revenu  = dict_niche[nom_file]['Revenus segemented'].value_counts().values  
+      
+        #plot pie of revenues 
+        ax2.pie(values_revenu, labels = labels_revenu ,autopct='%1.1f%%', colors =couleurs, startangle=50)   
+        #sns.distplot(dict_niche[nom_file]['Revenus segemented'],bins=10)     
+        ax2.set_title( 'Revenus ' ,size =20)  
+
+        # Histograme of evaluation
+        sns.distplot(dict_niche[nom_file]['Rating'],bins=10)
+        ax3.set_title( 'Notes ' ,size =20)    
+        st.pyplot(fig1)
+
+        #english
+        list_nom.append(nom_file)
+        moyenne_note.append(dict_niche[nom_file]['Rating'].median())
+        median_revenu_mensuel.append(dict_niche[nom_file]['Mo. Revenue'].median())
+        median_classement.append(dict_niche[nom_file]['Rank'].median())
+        median_avis.append(dict_niche[nom_file]['Reviews'].median())
+        median_listing.append(dict_niche[nom_file]['LQS'].median())
+    
+    df_conclustion =pd.DataFrame(data=[moyenne_note, median_revenu_mensuel, median_classement,median_avis, median_listing],columns=list_nom, index=['Rating','Mo. Revenue ','Rank ','Reviews','LQS '] ).transpose()
+    st.dataframe(df_conclustion)
+
+
+    
+      
+
+    
+    fig6, ax8  = plt.subplots(1, 1, figsize=(20,7))
+    sns.distplot(dict_niche[files[0].name[14:-4]]['Évaluation'],bins=10, label=files[0].name[14:-4])
+    sns.distplot(dict_niche[files[1].name[14:-4]]['Évaluation'],bins=10, label=files[1].name[14:-4])
+    sns.distplot(dict_niche[files[2].name[14:-4]]['Évaluation'],bins=10, label=files[2].name[14:-4])
+    sns.distplot(dict_niche[files[3].name[14:-4]]['Évaluation'],bins=10, label=files[3].name[14:-4])
+    ax8.set_title( 'Notes ' ,size =20)  
+    ax8.legend()  
+    st.pyplot(fig6)
+
+    
+    
+    fig7, ax8  = plt.subplots(1, 1, figsize=(20,7))
+    sns.distplot(dict_niche[files[3].name[14:-4]]['Revenus segementé'].value_counts(),bins=10, label=files[0].name[14:-4])
+    sns.distplot(dict_niche[files[1].name[14:-4]]['Revenus segementé'].value_counts(),bins=10, label=files[1].name[14:-4])
+    sns.distplot(dict_niche[files[2].name[14:-4]]['Revenus segementé'].value_counts(),bins=10, label=files[2].name[14:-4])
+    sns.distplot(dict_niche[files[3].name[14:-4]]['Revenus segementé'].value_counts(),bins=10, label=files[3].name[14:-4])
+    ax8.set_title( 'Notes ' ,size =20) 
+    #ax8.set_xlim(-10000,10000) 
+    ax8.legend()  
+    st.pyplot(fig7)
+# 4e pages 'Analyse Excel'
+elif add_selectbox == 'Analyse Excel':
+  uploaded_file = st.file_uploader("Choose a file") 
+  excel_file = pd.read_excel(uploaded_file, sheet_name=None, index_col=None)   
+
+
+  list_score_demande = []
+  list_score_competition = []
+  liste_score_JS_moy = []
+  list_nom_produit = []
+  liste_pays=[]
+  
+  for xl_name in excel_file:
+    #st.write(xl_name)
+    #st.write(excel_file[xl_name])  
+    st.header(xl_name)
+    df = encodage_level_alphabet(excel_file[xl_name])    
+    df_numerique = encodage_level_numerique(df)
+    df_numerique, filtre = nan_cleaning(df_numerique)
+
+    def deciamle(x):
+      return millify (x, precision=2)
+    
+    
+    
+    
+    
+    if filtre.count(True) !=0 :
+      list_score_demande.append(deciamle(df_numerique['DEMANDE'].mean()))
+      list_score_competition.append(deciamle(df_numerique['COMPETITION'].mean()))
+      liste_score_JS_moy.append(deciamle(df_numerique['SCORE '].mean()))
+      list_nom_produit.append(xl_name)
+
+
+      pays_choisi = excel_file[xl_name]['Pays'].apply(lambda x : x.upper())
+      st.write("**Chiffre basé sur les Marketplaces**:  ",'  ,  '.join(list(pays_choisi[filtre])))
+      liste_pays.append('  ,  '.join(list(pays_choisi[filtre])))
+      col1, col2, col3, col4 = st.columns(4)
+      col1.metric("Demand Score ", f'{millify (df_numerique["DEMANDE"].mean(axis=0))}')
+      col2.metric("Score compet",  f'{millify (df_numerique["COMPETITION"].mean(axis=0))}')
+      col3.metric("Score JS", millify (df_numerique["SCORE "].mean(axis=0), precision=1) )
+      col4.metric("Vol. de Vente moy/ mois ", f'{millify (df_numerique["Vol. de vente Mensuel"].sum(), precision =3)}' )
+    with st.expander("afficher les données "):
+      st.write(df_numerique)
+    
+    result = pd.DataFrame([list_score_competition, list_score_demande, liste_score_JS_moy, liste_pays],
+                          columns=(list_nom_produit),
+                          index=['score competition','Score demande','JS Score Moyen', 'Liste de pays '])
+
+  
+  st.write(result.T)
+
+      
+
+# 5e pages 'Analyse de groupe'
+elif add_selectbox == 'Analyse de groupe':
+  files =st.file_uploader('Choose files ', accept_multiple_files=True)
+  list_niche=[]
+  for elm in files:      
+    df = pd.read_csv(elm) 
+    df = df.iloc[:,3:]    
+    df = preprocessing(df, 'fr')       
+    list_niche.append(df)  
+   
+  df = pd.concat(list_niche)
+  df = df[~df['Évaluation'].isna()]
+  df = df[~df['Honoraires'].isna()]
+  
+  
+  st.write(f'Nbr d individu à analyser {df.shape[0]}') 
+  # plot configuration 
+  fig1,(ax1, ax2, ax3) = plt.subplots(1,3,figsize=(20, 7))
+  
+  plt.xticks(rotation = 90)
+  
+  labels_LQS = df['LQS'].apply(lambda x : str(x)).value_counts().sort_index().index
+  values_LQS  = df['LQS'].value_counts().sort_index().values       
+  # Plot Pie of listing score 
+  ax1.pie(values_LQS ,  labels=labels_LQS, autopct='%1.1f%%', colors=couleurs, startangle=50)    
+  ax1.set_title( 'Lising' ,size=20)   
+    
+  labels_revenu = df['Revenus segementé'].value_counts().sort_index().index
+  values_revenu  = df['Revenus segementé'].value_counts().values   
+        
+
+  #plot pie of revenues 
+  ax2.pie(values_revenu, labels = labels_revenu ,autopct='%1.1f%%', colors=couleurs, startangle=50)
+  #sns.distplot(dict_niche[nom_file]['Revenus segementé'],bins=10)           
+  ax2.set_title( 'Revenus ' ,size =20)
+        
+  # Histograme of evaluation 
+  sns.distplot(df['Évaluation'], bins=10)
+  ax3.set_title( 'Notes', size=20) 
+  st.pyplot(fig1)
+
+  fig4, ax6  = plt.subplots(1,1,figsize=(12,5))      
+  sns.scatterplot( df['Classement'],df['Ventes mensuelles'], hue=df['Revenus segementé'])
+  ax6.set_xlim(0,20000)
+  ax6.set_title('Nbr de vente en fonction du classement ',size =20)
+  st.pyplot(fig4)
+
+      
+  fig5, ax7  = plt.subplots(1,1,figsize=(12,5))      
+  sns.scatterplot(df['Ventes mensuelles'], df['Évaluation'], hue=df['Revenus segementé'])
+  ax7.set_title('influence du chiffre d affaire   M.vente(evalutation)',size =20)
+  st.pyplot(fig5)
+
+  
+
+  fig9, ax10  = plt.subplots(1,1,figsize=(12,5)) 
+
+
+
+  st.header('influence du chiffre d affaire avis(classement)')
+  sns.scatterplot( df['Classement'] ,df['Avis'], hue=df['Revenus segementé']) 
+  variation_x= st.slider('zoom du classement', min_value=0, max_value= int(df['Classement'].max()), value=int(df['Classement'].quantile(1)))
+  ax10.set_xlim(0,variation_x)
+  variation_y= st.slider('zoom de l\'avis', min_value=0, max_value= int(df['Avis'].max()), value=int(df['Avis'].quantile(1)))
+  ax10.set_ylim(-10,variation_y)
+  ax10.set_title('influence du chiffre d affaire avis(classement)',size =20)  
+  st.pyplot(fig9)
---
+++ features.py
@@ -0,0 +1,238 @@
+import requests
+import time as time
+import pandas as pd
+import numpy as np 
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import matplotlib.pyplot as plt
+import streamlit as st 
+
+
+
+
+
+
+
+
+
+# scrapt data from API 
+def scap_data(keyword, list_pays, nbr_pages, url, headers):  
+  dict_reponse={}
+  for pays in list_pays :    
+    for i in range(1,nbr_pages+1):
+      querystring = {"keywords":keyword,"region":pays,"page":str(i)}
+      response = requests.request("GET", url, headers=headers, params=querystring)
+      dict_reponse[pays+'_'+str(i)]=response
+      time.sleep(1)
+  return dict_reponse
+
+def merge_dicts(a, b):
+  m = a.copy()
+  m.update(b)
+  return m
+
+
+def cleaning(reponse_api):
+
+  #tranform from json to Dataframe
+  d = reponse_api.json()
+  df_search_result = pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in d.items() ]))
+  
+  #store the result in the dataframe
+  result_list=[]
+  for elm in df_search_result['results']:
+    result_list.append(elm)
+  df_result = pd.DataFrame(result_list)
+
+  # delate Nan in price and reviews columns
+  df_result = df_result[~df_result['price'].isna()]
+  df_result = df_result[~df_result['reviews'].isna()]
+
+  #cleaning empty field
+  liste_prix =[]
+  for elm in df_result['price'] :
+    if elm :
+      liste_prix.append(elm)
+  df_list_price = pd.DataFrame(liste_prix)
+
+
+  liste_reviews =[]
+  for elm in df_result['reviews'] :
+    if elm :
+      liste_reviews.append(elm)
+  df_liste_reviews = pd.DataFrame(liste_reviews)
+  df_finale= pd.concat([df_result,df_liste_reviews,df_list_price],axis=1)
+
+  #reshape to the util dataframe
+  df_utile = df_finale[['asin','title','prime','images','position','best_seller','avg_rating','num_ratings','amount','list_price','currency']]
+  df_utile = df_utile[~df_utile['amount'].isnull()]
+
+  #return a cleaned dataframe
+  return df_utile
+
+def affichage(data,country ,x ,y ):
+  #affichage plotly
+
+  fig = go.Figure(data=go.Scatter(x=data[x],
+                                y=data[y],
+                                mode='markers',
+                                marker_color=data['avg_rating'],
+                                text=data['title'])) # hover text goes here
+  fig.update_layout(title=f' Note en fonction du prix {country}')  
+  return fig
+
+
+def preprocessing(df, langue):
+  
+  
+  if langue == 'fr' :
+    
+    
+    # Traitement des Revenues
+    df["Honoraires"] = df["Honoraires"].replace('...', np.nan)
+    df["Honoraires"] = df["Honoraires"].astype(float)
+
+    # Traitement des ventes journalière 
+    df['Ventes journalières'] = df['Ventes journalières'].replace('...', np.nan)
+    df['Ventes journalières'] = df['Ventes journalières'].astype(float)
+
+    # Traitement des ventes mensuelle 
+    df['Revenus mensuels'] = df['Revenus mensuels'].replace('...', np.nan)
+    df['Revenus mensuels'] = df['Revenus mensuels'].astype(float)
+    # Traitement du temps 
+    # codage des ... en nan  Date First Available
+    df['Date de première disponibilité'] = df['Date de première disponibilité'].replace('...', np.nan)
+    
+    #supréssion des nan 
+    df=df[df['Date de première disponibilité'].isna()==False]
+
+    # encodage en datime 
+    df['Date de première disponibilité'] = pd.to_datetime(df['Date de première disponibilité'])
+    df = df[~df['Revenus mensuels'].isna()]
+
+    df["LQS"] = df["LQS"].replace('...', np.nan)
+    df["LQS"] = df["LQS"].astype(float)
+    df = df[~df['LQS'].isna()]
+    df['Revenus segementé'] = df['Revenus mensuels'].apply(lambda x : segmentation(x))
+    df['Ventes mensuelles'] = df['Ventes mensuelles'].astype(float)
+    df['Vendeurs'] = df['Vendeurs'].astype(float)
+
+    
+
+    
+  else :
+    
+    df['D. Sales'] = df['D. Sales'].replace('...', np.nan)
+    df['D. Sales'] = df['D. Sales'].astype(float)
+
+    # Traitement des ventes mensuelle 
+    df['Mo. Sales'] = df['Mo. Sales'].replace('...', np.nan)
+    df['Mo. Sales'] = df['Mo. Sales'].astype(float)
+
+    # Traitement des Revenues 
+    df["Mo. Revenue"] = df["Mo. Revenue"].replace('...', np.nan)
+    df["Mo. Revenue"] = df["Mo. Revenue"].astype(float)
+
+    # Traitement du temps 
+    # codage des ... en nan  Date First Available
+    df['Date First Available'] = df['Date First Available'].replace('...', np.nan)
+
+    #supréssion des nan 
+    df=df[df['Date First Available'].isna()==False]
+
+    # encodage en datime 
+    df['Date First Available'] = pd.to_datetime(df['Date First Available'])
+
+    # supression des NaN
+    df = df[~df['Mo. Revenue'].isna()]
+    df = df[~df['LQS'].isna()]
+    df['Revenus segemented'] = df['Mo. Revenue'].apply(lambda x : segmentation(x))
+
+  return df 
+
+def segmentation2(x) :
+    if x <= 100 :
+      x= 'inféreiur à 100'
+    elif (x>100) &  (x<=1000) :
+      x ='entre 100 a 1K'
+    elif (x>1000) &  (x<=5000) :
+      x = 'entre 1K a 5K'
+    elif (x>5000) &  (x<=10000) :
+      x = 'entre 5K a 10K'
+    elif (x>10000) &  (x<=20000) :
+      x = 'entre 10K a 20K'
+    elif (x>20000) &  (x<=50000) :
+      x = 'entre 20K a 50K'
+    elif x> 50000 :
+      x = 'supérieur à 50K'
+    else:
+      x = 'negatif'
+    return x
+
+def segmentation(x):
+  if x <= 1000 :
+    x= 'inféreiur à 1K'
+  elif (x>1000) &  (x<=10000) :
+    x ='entre 1K a 10K'   
+  elif (x>10000) &  (x<=50000) :
+    x = 'entre 10K a 50K'
+  elif x> 50000 :
+    x = 'supérieur à 50K'
+  else:
+    x = 'negatif'
+  return x
+
+def dectection_langue(list_colum):
+  text = " ".join(list_colum)
+  texte1 = "[\r {\r\"id\": \"1\",\r\"language\": \"fr\",\r\"text\": \""+text+"\"\r}\r]"
+  texte1 = texte1.replace("è",'e')
+  texte1 = texte1.replace("é",'e')
+  texte1 = texte1.replace("É",'e')  
+
+  #Api detection langue  
+  url = "https://language-detection4.p.rapidapi.com/language-detection"
+  payload = texte1
+  headers = {
+                'content-type': "application/json",
+                'accept': "application/json",
+                'x-rapidapi-host': "language-detection4.p.rapidapi.com",
+                'x-rapidapi-key': "f2b69e8ab2mshc6b37f3259eace5p104495jsnc2a8ef50efc6"
+              }
+
+  response = requests.request("POST", url, data=payload, headers=headers)
+  return response.json()[0]['detected_languages'][0]['prediction']
+
+def encodage_level_alphabet(df):
+      code_pour_dist_demande = {'medium': '2_medium','high': '1_high','low':'3_low'}
+      df['COMPETITION'] = df['COMPETITION'].map(code_pour_dist_demande)
+      df['DEMANDE'] = df['DEMANDE'].map(code_pour_dist_demande)
+      return df 
+
+def encodage_level_numerique(df):
+      code_pour_dist_demande = {'2_medium': (6), '1_high': (9), '3_low':(3)}
+      code_pour_dist_competition = {'2_medium': (6), '1_high': (3), '3_low':(9)}
+      df['COMPETITION'] = df['COMPETITION'].map(code_pour_dist_competition)
+      df['DEMANDE'] = df['DEMANDE'].map(code_pour_dist_demande)
+      return df
+
+def nan_cleaning(dataframe):
+    filtre=[]
+    for elm in  dataframe.isna().sum(axis=1):
+      if  (elm >= 0) & (elm <13) :
+        filtre.append(True)
+      if elm >= 13 :
+        filtre.append(False)
+     
+    return dataframe[filtre], filtre
+
+
+
+
+
+
+
+
+
+
+       
